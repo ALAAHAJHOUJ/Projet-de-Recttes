2d3e9ff303aa2fa98e2fdd97e3050c0bbfe4d8e1
hajhouj <alaa.spread@gmail.com>
2026-05-02 08:19:22 +0100

    projet presque finis

--- src/main/java/com/example/demo/Controller/ControllerRecette.java
+++ src/main/java/com/example/demo/Controller/ControllerRecette.java
@@ -3,11 +3,14 @@ package com.example.demo.Controller;
 
 import com.example.demo.FormatRecette.FormatRecette;
 import com.example.demo.Services.RecetteService;
+import com.example.demo.entites.Recette;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -56,13 +59,19 @@ public class ControllerRecette {
 
 
     @GetMapping("/api/recettes/{id}")
-    public ResponseEntity<FormatRecette> Consulter(@PathVariable int id){
-        FormatRecette r1=recetteService.chercher(id);
-        if (r1== null) {
+    public ResponseEntity<?> Consulter(@PathVariable int id){
+        boolean exister=recetteService.existe(id);
+        if (!exister) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
 
-        return ResponseEntity.ok(r1);
+        FormatRecette r1=recetteService.chercher(id);
+        return ResponseEntity.status(HttpStatus.OK).body(r1);
+    }
+
+    @GetMapping("/pagination/")
+    public List<Recette> tester(){
+        return recetteService.RecettePagine1(1,3);
     }
 
 
--- src/main/java/com/example/demo/Services/RecetteService.java
+++ src/main/java/com/example/demo/Services/RecetteService.java
@@ -7,10 +7,14 @@ import com.example.demo.entites.Ingredient;
 import com.example.demo.entites.Recette;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -83,6 +87,7 @@ public class RecetteService {
         }
 
         if(recette.getListIngredient()!=null){
+            System.out.println("exe");
             List<Ingredient> listIngr=new LinkedList<>();
             //d'abord on supprime tous les recettes
             for(Ingredient r:r1.getIngredients()){
@@ -113,9 +118,11 @@ public class RecetteService {
 
 
     public String  SupprimerRecette(int id){
-        Recette r1=recetteRepo.findById(id).orElseThrow(()->new RuntimeException("recette inexistante"));
+
+        Recette r1=recetteRepo.findById(id).orElseThrow(()->new RuntimeException("recette introuvable"));
 
         recetteRepo.delete(r1);
+
         return "suppression avec succes";
     }
 
@@ -127,6 +134,15 @@ public class RecetteService {
         Recette r1=recetteRepo.findById(id).orElseThrow(()->new RuntimeException("recette introuvable"));
         FormatRecette format=new FormatRecette();
         BeanUtils.copyProperties(r1,format);
+        List<FormatIngredient> list1=new LinkedList<>();
+
+        for(Ingredient ingr:r1.getIngredients()){
+            FormatIngredient forme1=new FormatIngredient();
+            BeanUtils.copyProperties(ingr,forme1);
+            list1.add(forme1);
+        }
+
+        format.setListIngredient(list1);
         return format;
     }
 
@@ -148,5 +164,14 @@ public class RecetteService {
         }else {
             return false;
         }
+    }
+
+
+    public List<Recette> RecettePagine1(int page,int size){
+        Page<Recette> page1 = recetteRepo.findAll(PageRequest.of(page,size));
+        for(Recette r1:page1.getContent()){
+            r1.getIngredients().clear();
+        }
+        return page1.getContent();
     }
 }
